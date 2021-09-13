40cf2f080974b12eec504fb1f8a6f69987384160
Pratik Daherwal <38158708+pratikdaherwal@users.noreply.github.com>
2021-09-13 23:38:20 +0530

    Update Task1.py
    Need to add more arguments for master, before it speaks only one sentence when 'your master' command given. Now it select random sentences.

--- Tasks/Task1.py
+++ Tasks/Task1.py
@@ -1,1 +1,12 @@
-# Task 1 - make random output accroding to input given by user (input only one and output is excepted 4-5)
+# Task 1 - make random master's name
+
+ elif 'your master' in query:
+
+            if platform == "win32" or "darwin":
+
+                list1 = ['Prateek, and Anshul are my masters', 'Anshul and Prateek created me, those are my matsers',
+                         'Prateek, and anshul are my master. For me, they are GOD', 'Anshul and Prateek']
+
+                list2 = random.choice(list1)
+
+                speak(list2)
